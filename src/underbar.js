5249070ad496be5ee1ab20dfa48b778d1344a61b
Andrew Chen <andrewchen.c44@gmail.com>
2017-09-09 18:30:36 -0700

    completed up to reduce

--- src/underbar.js
+++ src/underbar.js
@@ -136,6 +136,16 @@ for(var i = 0; i < collection.length; i++){
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+    if(Array.isArray(collection) === true){
+  var newArray = [];
+  
+for(var i = 0; i < collection.length; i++){
+  newArray[i] = iterator(collection[i], i, collection);
+} 
+return newArray;
+
+
+}
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
